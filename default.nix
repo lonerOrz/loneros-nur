653c229db166fba368be057fb52a85bc79e5a032
loner <loner.games@gmail.com>
2025-01-24 12:32:00 +0800

    add xdman8

--- default.nix
+++ default.nix
@@ -14,7 +14,6 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
+  # example-package = pkgs.callPackage ./pkgs/example-package { };
+  xdman8 = pkgs.libsForQt5.callPackage ./pkgs/xdman8 { };
 }
