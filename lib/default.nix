c8edc67ce3373126344741c1491ee20300b216d2
loner <2788892716@qq.com>
2025-07-05 04:45:56 +0800

    maintainers lonerOrz

--- lib/default.nix
+++ lib/default.nix
@@ -4,4 +4,12 @@ with pkgs.lib; {
   # Add your library functions here
   #
   # hexint = x: hexvals.${toLower x};
+  # 把 lonerOrz 加到 lib.maintainers 里
+  maintainers = maintainers // {
+    lonerOrz = {
+      name   = "lonerOrz";
+      email  = "lonerOrz@qq.com";
+      github = "lonerOrz";
+    };
+  };
 }
