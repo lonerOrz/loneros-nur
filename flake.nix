ba70c0858f160fe1fcdba3d9c2c049fb2e418017
loner <2788892716@qq.com>
2025-07-06 07:05:02 +0800

    rust-overlay

--- flake.nix
+++ flake.nix
@@ -9,40 +9,42 @@
     };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, ... }:
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    ...
+  }: let
+    systems = [
+      "x86_64-linux"
+      "i686-linux"
+      "x86_64-darwin"
+      "aarch64-linux"
+      "armv6l-linux"
+      "armv7l-linux"
+    ];
 
-    let
-      systems = [
-        "x86_64-linux"
-        "i686-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "armv6l-linux"
-        "armv7l-linux"
-      ];
+    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+  in {
+    legacyPackages = forAllSystems (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            rust-overlay.overlays.default
+          ];
+        };
 
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-    in
-    {
-      legacyPackages = forAllSystems (system:
-        let
-          # 在每个系统中导入带 overlay 的 pkgs
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              rust-overlay.overlays.default
-              (import ./overlays/rustPlatform.nix)
-            ];
-          };
+        legacyPkgs = import ./default.nix {
+          inherit pkgs;
+        };
+      in
+        legacyPkgs
+    );
 
-          legacyPkgs = import ./default.nix {
-            inherit pkgs;
-          };
-        in legacyPkgs
-      );
-
-      packages = forAllSystems (system:
+    packages = forAllSystems (
+      system:
         nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system}
-      );
-    };
+    );
+  };
 }
