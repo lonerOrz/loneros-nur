dfcb7a5dc5a8d11522dd6f73d62640478a1f55ae
GitHub Action <action@github.com>
2025-09-04 20:27:32 +0000

    Update Custom packages

--- pkgs/gemini-cli/default.nix
+++ pkgs/gemini-cli/default.nix
@@ -9,15 +9,15 @@
 }:
 let
   pname = "gemini-cli";
-  version = "0.2.2";
-  srcHash = "sha256-XLoviMM/gJ383D5GSQ2gWsp8P9EhxjQLbgQDmNr9FhI=";
-  npmDepsHash = "sha256-B/Bs6w+MJYZ+9EqSCqu9IXxyu7rpBctLNBqSGPSGhdE=";
+  version = "0.3.1";
+  srcHash = "sha256-cZpGEyMfl5GaZKVSOFgeS68s+T1kOAiEmpNOxJVasY4=";
+  npmDepsHash = "sha256-Ch+I46+GVu+JcZWrxIYrALbPrCFr8UBqT2FHbl4388c=";
 
   src = runCommand "gemini-cli-src-with-lock" { } ''
     mkdir -p $out
     tar -xzf ${
       fetchurl {
-        url = "https://registry.npmjs.org/@google/gemini-cli/-/gemini-cli-0.2.2.tgz";
+        url = "https://registry.npmjs.org/@google/gemini-cli/-/gemini-cli-0.3.1.tgz";
         hash = "${srcHash}";
       }
     } -C $out --strip-components=1
