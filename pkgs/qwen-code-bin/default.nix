4c8f6345e11313f28303830fee894b6d0efe8250
loner <2788892716@qq.com>
2025-09-14 14:16:01 +0800

    test12

---
+++ pkgs/qwen-code-bin/default.nix
@@ -0,0 +1,53 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchurl,
+  nodejs,
+}:
+let
+  owner = "QwenLM";
+  repo = "qwen-code";
+  asset = "gemini.js";
+  version = "0.0.11-nightly.8";
+in
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "qwen-code-bin";
+  inherit version;
+
+  src = fetchurl {
+    url = "https://github.com/${owner}/${repo}/releases/download/v${version}/${asset}";
+    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  };
+
+  phases = [
+    "installPhase"
+    "fixupPhase"
+  ];
+
+  strictDeps = true;
+
+  buildInputs = [ nodejs ];
+
+  installPhase = ''
+    runHook preInstall
+
+    install -D "$src" "$out/bin/qwen"
+
+    runHook postInstall
+  '';
+
+  passthru.updateScript = [
+    ./update-asset.sh
+    "${owner}/${repo}"
+    "${asset}"
+  ];
+
+  meta = {
+    description = "Coding agent that lives in the digital world";
+    homepage = "https://github.com/QwenLM/qwen-code";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ lonerOrz ];
+    mainProgram = "qwen";
+    platforms = lib.platforms.linux ++ lib.platforms.darwin;
+  };
+})
