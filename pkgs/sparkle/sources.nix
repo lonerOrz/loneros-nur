13c8206da61ac9745d27cfb23a1b97b5847ed46a
loner <2788892716@qq.com>
2026-01-16 02:19:20 +0800

    Fix: reupdate sparkle

--- pkgs/sparkle/sources.nix
+++ pkgs/sparkle/sources.nix
@@ -1,1 +1,10 @@
 {
+  aarch64-linux = {
+    url = "https://github.com/INKCR0W/sparkle/releases/download/1.6.19/sparkle-linux-1.6.19-arm64.deb";
+    hash = "sha256-4FHjc6d3eqCwNmFrmPuDQOoe2F8C71Ai7S8OcSbwbYs=";
+  };
+  x86_64-linux = {
+    url = "https://github.com/INKCR0W/sparkle/releases/download/1.6.19/sparkle-linux-1.6.19-amd64.deb";
+    hash = "sha256-LGKM6mwlQZBjCr4Qu8b97701CKaHHMLnPGVvhO+IapM=";
+  };
+}
