b5042cc65fd1c6ee1972f62e30f509b89bd08c26
loner <2788892716@qq.com>
2026-04-05 10:39:59 +0800

    nmag: init at 0.1.0

---
+++ pkgs/nmag/default.nix
@@ -0,0 +1,58 @@
+{
+  lib,
+  makeWrapper,
+  rustPlatform,
+  fetchFromGitHub,
+  pkg-config,
+  wayland,
+  wayland-protocols,
+  libGL,
+  vulkan-loader,
+  libxkbcommon,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "nmag";
+  version = "0.1.0";
+
+  src = fetchFromGitHub {
+    owner = "lonerOrz";
+    repo = "nmag";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-RricOfbvTD7C6z5KVRYRn5+KKHi9JLJ7bIdY+7X+CAM=";
+  };
+
+  cargoHash = "sha256-14k3+GpyKf7mNrnjKUTAsROyml7JUp4iXkLtdhBqEl4=";
+
+  nativeBuildInputs = [
+    pkg-config
+    makeWrapper
+  ];
+
+  buildInputs = [
+    wayland
+    wayland-protocols
+    libGL
+    vulkan-loader
+    libxkbcommon
+  ];
+
+  postInstall = ''
+    wrapProgram $out/bin/nmag \
+      --prefix LD_LIBRARY_PATH : ${
+        lib.makeLibraryPath [
+          libGL
+          vulkan-loader
+        ]
+      }
+  '';
+
+  meta = {
+    description = "Full-screen zoom for Wayland compositors";
+    homepage = "https://github.com/lonerOrz/nmag";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.linux;
+    maintainers = with lib.maintainers; [ lonerOrz ];
+    mainProgram = "nmag";
+  };
+})
