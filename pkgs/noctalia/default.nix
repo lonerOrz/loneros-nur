f74b8b6fe3cda1725e7d40cdcecda826ccbb1f6a
loner <2788892716@qq.com>
2026-05-03 14:12:28 +0800

    noctalia: init at 0-unstable-20260503-ad5383448

---
+++ pkgs/noctalia/default.nix
@@ -0,0 +1,98 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  callPackage,
+
+  meson,
+  ninja,
+  pkg-config,
+  wayland-scanner,
+  wayland,
+  wayland-protocols,
+  libGL,
+  libglvnd,
+  freetype,
+  fontconfig,
+  cairo,
+  pango,
+  libxkbcommon,
+  sdbus-cpp_2,
+  systemd,
+  pipewire,
+  pam,
+  curl,
+  libwebp,
+  glib,
+  polkit,
+}:
+
+let
+  current = lib.trivial.importJSON ./version.json;
+in
+stdenv.mkDerivation (finalAttrs: {
+  pname = "noctalia";
+  version = current.version;
+
+  src = fetchFromGitHub {
+    owner = "noctalia-dev";
+    repo = "noctalia-shell";
+    rev = current.rev;
+    hash = current.hash;
+  };
+
+  postPatch = ''
+    substituteInPlace meson.build \
+      --replace-fail "'-march=native', '-mtune=native'," ""
+  '';
+
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkg-config
+    wayland-scanner
+  ];
+
+  buildInputs = [
+    wayland
+    wayland-protocols
+    libGL
+    libglvnd
+    freetype
+    fontconfig
+    cairo
+    pango
+    libxkbcommon
+    sdbus-cpp_2
+    systemd
+    pipewire
+    pam
+    curl
+    libwebp
+    glib
+    polkit
+  ];
+
+  mesonBuildType = "release";
+
+  ninjaFlags = [ "-v" ];
+
+  passthru.updateScript = callPackage ../../utils/update.nix {
+    pname = finalAttrs.pname;
+    versionFile = "pkgs/noctalia/version.json";
+    fetchMetaCommand = "${(callPackage ../../utils/fetcher.nix { }).githubGit {
+      owner = "noctalia-dev";
+      repo = "noctalia-shell";
+      ref = "v5";
+    }}";
+  };
+
+  meta = {
+    description = "Lightweight Wayland shell and bar built directly on Wayland + OpenGL ES";
+    homepage = "https://github.com/noctalia-dev/noctalia-shell";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.linux;
+    mainProgram = "noctalia";
+    maintainers = with lib.maintainers; [ lonerOrz ];
+  };
+})
