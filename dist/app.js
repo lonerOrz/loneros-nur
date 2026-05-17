dcd22867f1b7cb645fe963fa5e73261f00135bca
loner <2788892716@qq.com>
2026-05-17 17:12:22 +0800

    Add word-wrap to Version column

--- dist/app.js
+++ dist/app.js
@@ -79,6 +79,24 @@ function escapeHtml(text) {
     .replaceAll('"', "&quot;");
 }
 
+function getInstallCommand(name) {
+  return `nix run github:lonerOrz/loneros-nur#${name}`;
+}
+
+function copyToClipboard(text, btn) {
+  navigator.clipboard.writeText(text).then(() => {
+    const originalHTML = btn.innerHTML;
+    btn.innerHTML = "Copied!";
+    btn.style.background = "rgba(126, 231, 135, 0.3)";
+    btn.style.borderColor = "#7ee787";
+    setTimeout(() => {
+      btn.innerHTML = originalHTML;
+      btn.style.background = "";
+      btn.style.borderColor = "";
+    }, 1500);
+  });
+}
+
 function normalizeLicense(license) {
   if (!license) {
     return "unknown";
@@ -120,6 +138,19 @@ function renderTable(packages) {
           </td>
 
           <td class="description">${escapeHtml(pkg.description || "")}</td>
+
+          <td>
+            <button
+              class="install-btn"
+              onclick="copyToClipboard('${escapeHtml(getInstallCommand(pkg.name))}', this)"
+            >
+              <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+                <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+              </svg>
+              nix run
+            </button>
+          </td>
 
           <td>
             ${
